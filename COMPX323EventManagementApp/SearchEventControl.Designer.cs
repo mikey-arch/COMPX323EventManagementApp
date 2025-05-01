744f8c0d3b56b3ddda94fbe49e3c53023960fd97
michael <mww11@students.waikato.ac.nz>
2025-05-01 22:36:33 +1200

    added filtering functionality and displaying events

--- COMPX323EventManagementApp/SearchEventControl.Designer.cs
+++ COMPX323EventManagementApp/SearchEventControl.Designer.cs
@@ -28,85 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBoxSort = new System.Windows.Forms.ComboBox();
+            this.comboBoxPrice = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.comboBoxFilter = new System.Windows.Forms.ComboBox();
+            this.comboBoxLocation = new System.Windows.Forms.ComboBox();
             this.listViewEvents = new System.Windows.Forms.ListView();
+            this.comboBoxCategory = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxRestriction = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerMonth = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.labelEvents = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // comboBoxSort
+            // comboBoxPrice
             // 
-            this.comboBoxSort.FormattingEnabled = true;
-            this.comboBoxSort.Items.AddRange(new object[] {
-            "Date",
-            "Price($$$-$)",
-            "Prrice($-$$$)"});
-            this.comboBoxSort.Location = new System.Drawing.Point(44, 55);
-            this.comboBoxSort.Name = "comboBoxSort";
-            this.comboBoxSort.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxSort.TabIndex = 1;
+            this.comboBoxPrice.FormattingEnabled = true;
+            this.comboBoxPrice.Location = new System.Drawing.Point(483, 41);
+            this.comboBoxPrice.Name = "comboBoxPrice";
+            this.comboBoxPrice.Size = new System.Drawing.Size(104, 21);
+            this.comboBoxPrice.TabIndex = 1;
+            this.comboBoxPrice.SelectedIndexChanged += new System.EventHandler(this.comboBoxPrice_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 39);
+            this.label1.Location = new System.Drawing.Point(483, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.Size = new System.Drawing.Size(31, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Sort";
+            this.label1.Text = "Price";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(226, 39);
+            this.label2.Location = new System.Drawing.Point(129, 25);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.Size = new System.Drawing.Size(48, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Filter";
+            this.label2.Text = "Location";
             // 
-            // comboBoxFilter
+            // comboBoxLocation
             // 
-            this.comboBoxFilter.FormattingEnabled = true;
-            this.comboBoxFilter.Items.AddRange(new object[] {
-            "Date",
-            "Price($$$-$)",
-            "Prrice($-$$$)"});
-            this.comboBoxFilter.Location = new System.Drawing.Point(229, 55);
-            this.comboBoxFilter.Name = "comboBoxFilter";
-            this.comboBoxFilter.Size = new System.Drawing.Size(121, 21);
-            this.comboBoxFilter.TabIndex = 4;
+            this.comboBoxLocation.FormattingEnabled = true;
+            this.comboBoxLocation.Location = new System.Drawing.Point(132, 41);
+            this.comboBoxLocation.Name = "comboBoxLocation";
+            this.comboBoxLocation.Size = new System.Drawing.Size(99, 21);
+            this.comboBoxLocation.TabIndex = 4;
+            this.comboBoxLocation.SelectedIndexChanged += new System.EventHandler(this.comboBoxFilter_SelectedIndexChanged);
             // 
             // listViewEvents
             // 
             this.listViewEvents.HideSelection = false;
-            this.listViewEvents.Location = new System.Drawing.Point(47, 137);
+            this.listViewEvents.Location = new System.Drawing.Point(19, 88);
             this.listViewEvents.Name = "listViewEvents";
-            this.listViewEvents.Size = new System.Drawing.Size(734, 321);
+            this.listViewEvents.Size = new System.Drawing.Size(596, 252);
             this.listViewEvents.TabIndex = 5;
             this.listViewEvents.UseCompatibleStateImageBehavior = false;
+            this.listViewEvents.DoubleClick += new System.EventHandler(this.listViewEvents_DoubleClick);
+            // 
+            // comboBoxCategory
+            // 
+            this.comboBoxCategory.FormattingEnabled = true;
+            this.comboBoxCategory.Location = new System.Drawing.Point(237, 41);
+            this.comboBoxCategory.Name = "comboBoxCategory";
+            this.comboBoxCategory.Size = new System.Drawing.Size(99, 21);
+            this.comboBoxCategory.TabIndex = 8;
+            this.comboBoxCategory.SelectedIndexChanged += new System.EventHandler(this.comboBoxCategory_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(344, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Restriction";
+            // 
+            // comboBoxRestriction
+            // 
+            this.comboBoxRestriction.FormattingEnabled = true;
+            this.comboBoxRestriction.Location = new System.Drawing.Point(342, 41);
+            this.comboBoxRestriction.Name = "comboBoxRestriction";
+            this.comboBoxRestriction.Size = new System.Drawing.Size(135, 21);
+            this.comboBoxRestriction.TabIndex = 11;
+            this.comboBoxRestriction.SelectedIndexChanged += new System.EventHandler(this.comboBoxRestriction_SelectedIndexChanged);
+            // 
+            // dateTimePickerMonth
+            // 
+            this.dateTimePickerMonth.Checked = false;
+            this.dateTimePickerMonth.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerMonth.Location = new System.Drawing.Point(21, 41);
+            this.dateTimePickerMonth.Name = "dateTimePickerMonth";
+            this.dateTimePickerMonth.ShowUpDown = true;
+            this.dateTimePickerMonth.Size = new System.Drawing.Size(105, 20);
+            this.dateTimePickerMonth.TabIndex = 12;
+            this.dateTimePickerMonth.ValueChanged += new System.EventHandler(this.dateTimePicker_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Date";
+            // 
+            // labelEvents
+            // 
+            this.labelEvents.AutoSize = true;
+            this.labelEvents.Location = new System.Drawing.Point(18, 72);
+            this.labelEvents.Name = "labelEvents";
+            this.labelEvents.Size = new System.Drawing.Size(45, 13);
+            this.labelEvents.TabIndex = 14;
+            this.labelEvents.Text = "Results:";
             // 
             // SearchEventControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelEvents);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePickerMonth);
+            this.Controls.Add(this.comboBoxRestriction);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxCategory);
             this.Controls.Add(this.listViewEvents);
-            this.Controls.Add(this.comboBoxFilter);
+            this.Controls.Add(this.comboBoxLocation);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBoxSort);
+            this.Controls.Add(this.comboBoxPrice);
             this.Name = "SearchEventControl";
-            this.Size = new System.Drawing.Size(897, 584);
+            this.Size = new System.Drawing.Size(640, 446);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ComboBox comboBoxSort;
+        private System.Windows.Forms.ComboBox comboBoxPrice;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBoxFilter;
+        private System.Windows.Forms.ComboBox comboBoxLocation;
         private System.Windows.Forms.ListView listViewEvents;
+        private System.Windows.Forms.ComboBox comboBoxCategory;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxRestriction;
+        private System.Windows.Forms.DateTimePicker dateTimePickerMonth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label labelEvents;
     }
 }
--- COMPX323EventManagementApp/SearchEventControl.cs
+++ COMPX323EventManagementApp/SearchEventControl.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Linq;
-using COMPX323EventManagementApp.Models;
 using Oracle.ManagedDataAccess.Client;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace COMPX323EventManagementApp
 {
@@ -20,49 +9,258 @@ namespace COMPX323EventManagementApp
         public SearchEventControl()
         {
             InitializeComponent();
+            LoadComboBoxes();
+            DisplayEvents();
+        }
 
+        //loads all comboboxes with appropriate filters names
+        private void LoadComboBoxes()
+        {
             try
             {
+                dateTimePickerMonth.CustomFormat = "MMMM yyyy";
+                dateTimePickerMonth.Value = DateTime.Now;
+                comboBoxPrice.Items.Add("Asc");
+                comboBoxPrice.Items.Add("Desc");
+                comboBoxPrice.Items.Add("Free");
+                comboBoxPrice.SelectedIndex = 0;
+
+                using(var conn = DbConfig.GetConnection())
+                {
+                    conn.Open();
+
+                    //add locations city names from venue table 
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "select distinct city from venue order by city asc";
+                        comboBoxLocation.Items.Add("All Locations");
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read()) 
+                            {
+                                comboBoxLocation.Items.Add(reader.GetString(0));
+                            }
+                        }
+                        comboBoxLocation.SelectedIndex = 0;
+                    }
+
+                    //add in each category from category table
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "select cname from category_tag order by cname asc";
+                        comboBoxCategory.Items.Add("All Categories");
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read()) 
+                            {
+                                comboBoxCategory.Items.Add(reader.GetString(0));
+                            }
+                        }
+                        comboBoxCategory.SelectedIndex = 0;
+                    }
+
+                    //fill restrictions combobox with restrictions from restrictions table
+                    using (var cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "select rname from Restrictions order by rname asc";
+                        comboBoxRestriction.Items.Add("All Restrictions");
+                        
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                comboBoxRestriction.Items.Add(reader.GetString(0));
+                            }
+                        }
+                        comboBoxRestriction.SelectedIndex = 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading filters: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //displays updated filtered events in the listview
+        private void DisplayEvents()
+        {
+            try
+            {
+                // Clear existing items
+                listViewEvents.Items.Clear();
+
+                // Make sure ListView is properly set up
+                if (listViewEvents.Columns.Count == 0)
+                {
+                    listViewEvents.View = View.Details;
+                    listViewEvents.FullRowSelect = true;
+                    listViewEvents.GridLines = true;
+
+                    listViewEvents.Columns.Add("Event Name", 180);
+                    listViewEvents.Columns.Add("Date", 100);
+                    listViewEvents.Columns.Add("Time", 80);
+                    listViewEvents.Columns.Add("Venue", 150);
+                    listViewEvents.Columns.Add("Price", 80);
+                    listViewEvents.Columns.Add("Category", 100);
+                    listViewEvents.Columns.Add("Restrictions", 120);
+                }
+
                 using (var conn = DbConfig.GetConnection())
                 {
                     conn.Open();
 
-                    // Search for events and display all events details when form opens
+                    // Build the query with all the filters
+                    string query = @"
+                    select distinct e.ename, ei.event_date, ei.time, v.vname, v.city, ei.price, ct.cname, r.rname
+                    from event e
+                    join event_instance ei on e.ename = ei.ename
+                    join venue v on ei.vname = v.vname
+                    left join has_a ha on e.ename = ha.ename
+                    left join category_tag ct on ha.cname = ct.cname
+                    left join has h on e.ename = h.ename
+                    left join restrictions r on h.rname = r.rname
+                    where 1=1";
+
+                    // handles the price combo box filtering selection
+                    string priceFilter = comboBoxPrice.SelectedItem?.ToString();
+                    if (!string.IsNullOrEmpty(priceFilter))
+                    {
+                        if (priceFilter == "Free")
+                        {
+                            query += " and ei.price = 0";
+                        }
+                    }
+
+                    // handles location filter
+                    string locationFilter = comboBoxLocation.SelectedItem?.ToString();
+                    if (!string.IsNullOrEmpty(locationFilter) && locationFilter != "All Locations")
+                    {
+                        query += " and v.city = :location";
+                    }
+
+                    // hanldes category filter
+                    string categoryFilter = comboBoxCategory.SelectedItem?.ToString();
+                    if (!string.IsNullOrEmpty(categoryFilter) && categoryFilter != "All Categories")
+                    {
+                        query += " and ct.cname = :category";
+                    }
+
+                    // handles restrictions filter
+                    string restrictionFilter = comboBoxRestriction.SelectedItem?.ToString();
+                    if (!string.IsNullOrEmpty(restrictionFilter) && restrictionFilter != "All Restrictions")
+                    {
+                        query += " and r.rname = :restriction";
+                    }
+
+                    //add date filtering
+                    DateTime selectedDate = dateTimePickerMonth.Value;
+                    query += " and extract(month from ei.event_date) = :month and extract(year from ei.event_date) = :year";
+
+                    //add sorting filter
+                    if (priceFilter == "Asc")
+                    {
+                        query += " order by ei.price asc, ei.event_date asc";
+                    }
+                    else if (priceFilter == "Desc")
+                    {
+                        query += " order by ei.price desc, ei.event_date asc";
+                    }
+                    else
+                    {
+                        query += " order by ei.event_date asc, ei.time asc";
+                    }
+
                     using (var cmd = conn.CreateCommand())
                     {
-                        
-                        cmd.CommandText = "select ename, description, creation_date, company_club from event";
+                        cmd.CommandText = query;
+
+                        // add location, category, restriction and date parameters
+                        if (!string.IsNullOrEmpty(locationFilter) && locationFilter != "All Locations")
+                        {
+                            cmd.Parameters.Add("location", OracleDbType.Varchar2).Value = locationFilter;
+                        }
+
+                        if (!string.IsNullOrEmpty(categoryFilter) && categoryFilter != "All Categories")
+                        {
+                            cmd.Parameters.Add("category", OracleDbType.Varchar2).Value = categoryFilter;
+                        }
+
+                        if (!string.IsNullOrEmpty(restrictionFilter) && restrictionFilter != "All Restrictions")
+                        {
+                            cmd.Parameters.Add("restriction", OracleDbType.Varchar2).Value = restrictionFilter;
+                        }
+
+                        cmd.Parameters.Add("month", OracleDbType.Int32).Value = selectedDate.Month;
+                        cmd.Parameters.Add("year", OracleDbType.Int32).Value = selectedDate.Year;
 
                         using (var reader = cmd.ExecuteReader())
                         {
-                            // Setup listView
-                            listViewEvents.View = View.Details;
-                            listViewEvents.Columns.Add("Name", 150);
-                            listViewEvents.Columns.Add("Description", 300);
-                            listViewEvents.Columns.Add("Date", 100);
-                            listViewEvents.Columns.Add("Club", 120);
+                            bool foundResults = false;
 
-                            // Add items to ListView
                             while (reader.Read())
                             {
-                                var item = new ListViewItem(reader.GetString(0));
-                                item.SubItems.Add(reader.GetString(1));
-                                item.SubItems.Add(reader.GetDateTime(2).ToShortDateString());
-                                item.SubItems.Add(reader.GetString(3));
-                                listViewEvents.Items.Add(item);
+                                foundResults = true;
 
+                                // create list view item with event details, adding eventname, date, time, venue, price, category and restrictiosn
+                                ListViewItem item = new ListViewItem(reader.GetString(0)); 
+
+                                DateTime eventDate = reader.GetDateTime(1);
+                                item.SubItems.Add(eventDate.ToString("dd-MM-yyyy"));
+
+                                DateTime eventTime = reader.GetDateTime(2);
+                                item.SubItems.Add(eventTime.ToString("HH:mm"));
+
+                                item.SubItems.Add(reader.GetString(3));
+
+                                decimal price = reader.GetDecimal(5);
+                                item.SubItems.Add(price.ToString("C"));
+
+                                item.SubItems.Add(reader.GetString(6));
+                                item.SubItems.Add(reader.GetString(7));
+                                listViewEvents.Items.Add(item);
                             }
+                            labelEvents.Text = foundResults ? $"Found {listViewEvents.Items.Count} events" : "No events found matching your criteria";
                         }
                     }
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Error displaying events: " + ex.Message, "Database Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
 
-            
-            
+        private void comboBoxFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayEvents();
+        }
+        private void comboBoxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayEvents();
+        }
+        private void comboBoxRestriction_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayEvents();
+        }
+        private void comboBoxPrice_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DisplayEvents();
+        }
+        private void dateTimePicker_ValueChanged(object sender, EventArgs e)
+        {
+            DisplayEvents();
+        }
+
+        //should handle if selected then showeventdetails and go to that events details
+        private void listViewEvents_DoubleClick(object sender, EventArgs e)
+        {
+            if (listViewEvents.SelectedItems.Count > 0)
+            {
+                MessageBox.Show("opens this event page");
+            }
         }
     }
 }
